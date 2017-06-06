c4a79fac22a45b47a0f875c5cf044055113498c0
David Nix <davidnix81@gmail.com>
2017-06-06 09:21:39 -0600

    draw marketing and build number

--- main.go
+++ main.go
@@ -5,21 +5,76 @@ import (
 
 	"image"
 
+	"flag"
+	"strings"
+
+	"golang.org/x/image/font/basicfont"
 	"golang.org/x/image/font/inconsolata"
 	gg "gopkg.in/fogleman/gg.v1"
 )
 
+const Version = "0.1.0"
+
+var (
+	mversion string
+	buildNum string
+)
+
+func init() {
+	log.SetFlags(0)
+
+	flag.StringVar(&mversion, "mversion", "", "Marketing version (ex: 1.3.4)")
+	flag.StringVar(&buildNum, "b", "", "Build number")
+}
+
 func main() {
+	flag.Parse()
+
+	if strings.Contains(flag.Arg(0), "version") {
+		log.Println(Version)
+		return
+	}
+
 	img, err := gg.LoadImage("/users/david/desktop/icon-180.png")
 	exitIf("could not open file", err)
-
-	betaText := betaContext(img)
-
 	dc := gg.NewContextForImage(img)
-	dc.DrawImage(betaText.Image(), 0, img.Bounds().Max.Y-betaText.Height())
-	err = gg.SavePNG("/users/david/desktop/icon-transformed.png", dc.Image())
+	drawMarketingVersion(dc)
+	drawBuildNumber(dc)
 
+	err = gg.SavePNG("/users/david/desktop/icon-transformed.png", dc.Image())
 	exitIf("could not save png", err)
+}
+
+func drawMarketingVersion(parent *gg.Context) {
+	w, h := versionDimensions(parent)
+	vc := gg.NewContext(w, h)
+	vc.SetHexColor("#555555")
+	vc.Clear()
+
+	vc.SetFontFace(basicfont.Face7x13)
+	vc.SetRGB(1, 1, 1)
+	vc.DrawStringAnchored(mversion, float64(w/2), float64(h/2), 0.5, 0.5)
+
+	parent.DrawImage(vc.Image(), 0, 0)
+}
+
+func drawBuildNumber(parent *gg.Context) {
+	w, h := versionDimensions(parent)
+	vc := gg.NewContext(w, h)
+	vc.SetHexColor("#f48041")
+	vc.Clear()
+
+	vc.SetFontFace(basicfont.Face7x13)
+	vc.SetRGB(1, 1, 1)
+	vc.DrawStringAnchored(buildNum, float64(w/2), float64(h/2), 0.5, 0.5)
+
+	parent.DrawImage(vc.Image(), parent.Width()/2, 0)
+}
+
+func versionDimensions(dc *gg.Context) (int, int) {
+	width := dc.Image().Bounds().Max.X / 2
+	height := int(float64(dc.Image().Bounds().Max.Y)*0.2) - 1
+	return width, height
 }
 
 func betaContext(img image.Image) *gg.Context {
